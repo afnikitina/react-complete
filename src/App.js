12810de5165f62617d25de615c0b1475ab33503b
Asya Nikitina <a.f.nikitina@gmail.com>
2018-11-11 15:16:22 -0700

    Modified the state and event handler in App.js

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHander = () => {
+  switchNameHander = (newName, newAge) => {
     // console.log('Was clicked!');
     // this.state.person[0].name = 'Maximiliam';
     this.setState({
       persons: [
-        { name: 'Maximiliam', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Sarah', age: 29 },
         { name: 'Caleb', age: 17 },
-        { name: 'Jude', age: 30 }
+        { name: 'Jude', age: newAge }
       ]
     })
   }
@@ -30,11 +30,24 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I am a React App!</h1>
-        <button onClick={this.switchNameHander}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobby is music</Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <button onClick={() => this.switchNameHander('Marlon', 31)}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+        />
+        <Person 
+          name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}
+            click={this.switchNameHander.bind(this, 'Ryan', 32)}
+        >My hobby is music</Person>
+        <Person 
+          name={this.state.persons[3].name} 
+          age={this.state.persons[3].age}
+        />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi there!'));
