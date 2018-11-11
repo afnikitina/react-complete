8e91d440f4f99e678d9941a2354335cbc23126dd
Asya Nikitina <a.f.nikitina@gmail.com>
2018-11-11 16:03:17 -0700

    Modified App.js by adding inline style to the button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
   state = {
     persons: [
@@ -26,11 +27,34 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Joy', age: 34 },
+        { name: 'Chelsea', age: 39 },
+        { name: 'Caleb', age: 17 },
+        { name: event.target.value, age: 29 }
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'green',
+      font: 'inherit',
+      border: '3px solid red',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I am a React App!</h1>
-        <button onClick={() => this.switchNameHander('Marlon', 31)}>Switch Name</button>
+        <div>
+          <button 
+            style={style}
+            onClick={() => this.switchNameHander('Marlon', 31)}>Switch Name</button>
+        </div>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
@@ -47,6 +71,7 @@ class App extends Component {
         <Person 
           name={this.state.persons[3].name} 
           age={this.state.persons[3].age}
+          changed={this.nameChangedHandler}
         />
       </div>
     );
